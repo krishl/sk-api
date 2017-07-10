f9297ba3666feda23ec99efd322a3b42b5a37d62
github username <krishel.lasam@gmail.com>
2017-07-10 00:01:08 -0400

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+facewash = Product.create(
+  name: "Face Wash",
+  brand: "Face wash brand"
+)
+facewash.ingredients.create(name: "Water")
+facewash.ingredients.create(name: "Soap")
+
+lotion = Product.create(
+  name: "Lotion",
+  brand: "Lotion Brand"
+)
+lotion.ingredients.create(name: "Water")
+lotion.ingredients.create(name: "Fragrance")
