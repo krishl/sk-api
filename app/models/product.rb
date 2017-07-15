729db1fd991c3909440a350b81970ea606081791
github username <krishel.lasam@gmail.com>
2017-07-15 04:33:31 -0400

    define custom attribute writer on product model for ing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   has_many :product_ingredients
   has_many :ingredients, through: :product_ingredients
+
+  def ings=(string)
+    ingredients = string.split(/\s*,\s*/)
+    ingredients.each do |ingredient|
+      new_ingredient = Ingredient.find_or_create_by(name: ingredient)
+      self.ingredients << new_ingredient
+    end
+  end
 end
