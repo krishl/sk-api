1900754d0e09f8f6f7b758b0d9e0960ae4bdb3d5
github username <krishel.lasam@gmail.com>
2017-07-15 04:31:54 -0400

    redo products_controller for jbuilder

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    render json: Product.includes(:ingredients), include: ['ingredients']
+    @products = Product.all
+    @products
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render @product
+    else
+      render json: {status: 500, err: 'Unable to save product'}
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :brand, :ings)
   end
 end
