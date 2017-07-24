b05bdeb3df570031ce5b586b1ff95d6a587b75d1
github username <krishel.lasam@gmail.com>
2017-07-24 19:10:38 -0400

    define upvote and params in controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -13,9 +13,25 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def upvote
+    @product = Product.find_by(upvote_params)
+    @product.upvote += 1
+    if @product.save
+       @products = Product.all
+       @products = @products.reverse
+       @products
+    else
+      render json: {status: 500, err: 'Unable to upvote product'}
+    end
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :brand, :ings)
+    params.require(:product).permit(:name, :brand, :ings, :upvote)
+  end
+
+  def upvote_params
+    params.require(:product).permit(:id)
   end
 end
